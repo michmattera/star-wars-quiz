8f97e5d040c62868c732482c53be5e46d8287ae0
michmattera <michellemattera97@gmail.com>
2023-01-13 09:34:40 +0000

    Add empty function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log('hello');
+//Starting to write all function of the game for better structure
+function userName(){}
+
+function easyModality(){}
+
+function mediumModality(){}
+
+function difficultModality(){}
+
+function expertModality(){}
+
+function chooseDarkSide(){}
+
+function chooseLightSide(){}
+
+function runGame(){}
+
+function nextQuestion(){}
+
+function getScore(){}
+
+function incrementPositiveAnswer(){}
+
+function incrementNegativeAnswer(){}
+
+function calcolateFinalScore(){}
+
+function getSound(){}
+
+function getLeaderboard(){}
+
