261965611f5db98de4ebb4cd6b8a3b6705298a96
michmattera <michellemattera97@gmail.com>
2023-01-13 16:44:36 +0000

    Add button in main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-//when page is loaded open the modal
-document.addEventListener("DOMContentLoaded" , function(){
-    $(window).on('load', function() {
-        $('#myModal').modal('show');
-    });
-})
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
+$(document).ready(function(){
+  $('#myModal').modal('show');
+});
 
 //Starting to write all function of the game for better structure
 function userName(){}
