44f3b545fb2790c89a28c851177a654169ea15e9
michmattera <michellemattera97@gmail.com>
2023-01-16 16:08:44 +0000

    Change function to run light and dark games

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,6 @@
 //});
 
 
-
-let darkQuestions = [questionOnee, questionTwoo, questionThreee];
-
-//test question for running light game
-let lightQuestions = [questionOne, questionTwo, questionThree];
-
-
 //Starting to write all function of the game for better structure
 function userName() {}
 
@@ -32,7 +25,20 @@ function incrementNegativeAnswer() {}
 function calcolateFinalScore() {}
 
 //Wait for the DOM to finish loading before running the game
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded" , function(){
+  let buttons = document.getElementsByTagName('button');
+
+  for(let button of buttons){
+      button.addEventListener('click', function(){
+          if(this.getAttribute("data-type") === "light"){
+              runLightGame();
+          } else {
+              runDarkGame();
+          }
+      })
+  }
+});
+
 
 /**
  * When chose side in chooseDarkSide or chooseLightSide
@@ -40,32 +46,14 @@ document.addEventListener("DOMContentLoaded", function () {});
  */
 function runDarkGame() {
   console.log('dark game');
+  
 }
 
 function runLightGame() {
   console.log('Light game');
+  
 }
 
-/**
- * When user click darkSide 
- * then runGame
- */
-function chooseDarkSide() {
-  let darkSide = document.getElementsByClassName('dark-side');
-  console.log('Dark side');
-  runDarkGame()
-}
-
-/**
- * When user click lightSide 
- * then runGame
- */
-function chooseLightSide() {
-  let lightSide = document.getElementsByClassName('light-side');
-  console.log('Light side');
-  console.log(questions[0]);
-  runLightGame()
-}
 
 //https://stackoverflow.com/questions/9419263/how-to-play-audio
 //Play audio and user decide when to play or stop music
