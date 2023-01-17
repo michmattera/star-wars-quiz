c52d2679f833d7d84c05ad9e85e3c0518bfb2e4f
michmattera <michellemattera97@gmail.com>
2023-01-17 11:02:32 +0000

    Add hide property to light side and start to write variables for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,51 +24,73 @@ function incrementNegativeAnswer() {}
 
 function calcolateFinalScore() {}
 
-//Wait for the DOM to finish loading before running the game
-document.addEventListener("DOMContentLoaded" , function(){
-  let buttons = document.getElementsByTagName('button');
+// variables to play light game
+let lightQuestions = [{
+    question: "What is the capital of United Kingdom?",
+    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+    answer: 2
+  },
 
-  for(let button of buttons){
-      button.addEventListener('click', function(){
-          if(this.getAttribute("data-type") === "light"){
-              runLightGame();
-          } else {
-              runDarkGame();
-          }
-      })
+  {
+    question: "What is the capital of United States?",
+    choices: ["California", "New York", "Miami", "Florida"],
+    answer: 1
   }
-});
+];
+//Declare variables for game
+const questionContainerElement = document.getElementById('question-container');
+let chosenSide = document.getElementById('light-side');
+
 
 
 /**
- * When chose side in chooseDarkSide or chooseLightSide
+ * When chosen side
+ * runLightGame or runDarkGame
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (this.getAttribute("data-type") === "light") {
+        runLightGame();
+      } else if (this.getAttribute("data-type") === "dark") {
+        runDarkGame();
+      }
+    })
+  }
+});
+
+/**
+ * When chose side in runDarkGame or runLightGame
  * displaylightQuestions or displayDarkQuestions
  */
 function runDarkGame() {
   console.log('dark game');
-  
+
 }
 
 function runLightGame() {
-  console.log('Light game');
-  
+  console.log('light game')
+  chosenSide.classList.add('hide')
+  questionContainerElement.classList.remove('hide')
+
 }
 
 
 //https://stackoverflow.com/questions/9419263/how-to-play-audio
 //Play audio and user decide when to play or stop music
 
-
-  function togglePlay() {
-    let audioElem = document.getElementById('player');
-    if (audioElem.paused) {
-       audioElem.className =("fa-solid fa-volume-high");
-       audioElem.play();
-    } else {
-       audioElem.className =("fa-solid fa-volume-xmark");
-       audioElem.pause();
-    }
- }
+function togglePlay() {
+  let audioElem = document.getElementById('player');
+  if (audioElem.paused) {
+    audioElem.className = ("fa-solid fa-volume-high");
+    audioElem.play();
+  } else {
+    audioElem.className = ("fa-solid fa-volume-xmark");
+    audioElem.pause();
+  }
+}
 
 
 //isPlaying ? audio.pause() : audio.play();
