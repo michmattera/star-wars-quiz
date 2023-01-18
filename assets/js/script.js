c047889807f224e56d7b0e84338f70cc0d08272a
michmattera <michellemattera97@gmail.com>
2023-01-18 12:18:50 +0000

    Change style for footer , and write new varable to get the que to see question in showQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,13 +66,14 @@ let lightQuestions = [{
 
 
 //Declare variables for game
-const questionContainerElement = document.getElementById('question-container');
+const gameAreaElement = document.getElementById('game-area');
+let questionContainer = document.getElementById('question');
 let chosenLightSide = document.getElementById('light-side');
 let chosenDarkSide = document.getElementById('dark-side');
-
-
 let shuffledLightQuestions = lightQuestions.sort[Math.floor(Math.random() * lightQuestions.length)];
 let currentQuestionsIndex = 0
+//try to write i, as well inside not working, as well if i just add lenght of lightquestions question
+let questionNumber = 0;
 
 /**
  * When chosen side
@@ -94,39 +95,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /**
  * When chose side in runDarkGame
- * displayDarkQuestions, nextQuestions and hide chosenLightSide/chosenDarkSide
+ * display DarkQuestions, nextQuestions and hide chosenLightSide/chosenDarkSide
  */
 function runDarkGame() {
   console.log('dark game');
   chosenDarkSide.classList.add('hide')
   chosenLightSide.classList.add('hide')
-  questionContainerElement.classList.remove('hide')
+  gameAreaElement.classList.remove('hide')
 
   nextQuestion()
 }
 
 /**
  * When chose side in runLightGame
- * displayLightQuestions, nextQuestions and hide chosenLightSide/chosenDarkSide
+ * display LightQuestions, nextQuestions and hide chosenLightSide/chosenDarkSide
  */
 function runLightGame() {
   console.log('light game')
   chosenLightSide.classList.add('hide')
   chosenDarkSide.classList.add('hide')
-  questionContainerElement.classList.remove('hide')
+  gameAreaElement.classList.remove('hide')
   showQuestion()
-  nextQuestion()
 
 }
-
+/**
+ * get nextQuestion from the current question and use shuffledLightQuestions to do it randomly
+ */
 function nextQuestion() {
   showQuestion(shuffledLightQuestions[currentQuestionsIndex])
 }
 
-function showQuestion(lightQuestions) {
-  questionContainerElement.innerText = lightQuestions.question[i];
-}
 
+/**
+ * show the questions in the question container
+ */
+function showQuestion() {
+  questionContainer.innerText = lightQuestions[questionNumber].question;
+  console.log(lightQuestions[questionNumber].question);
+
+}
 
 //https://stackoverflow.com/questions/9419263/how-to-play-audio
 //Play audio and user decide when to play or stop music
